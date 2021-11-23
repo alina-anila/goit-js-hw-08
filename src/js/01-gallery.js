cc0c5661f562ec9fa48825217ac8be5ba8a2647f
Alina Boiko <alina2boiko@gmail.com>
2021-11-23 19:25:55 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.innerHTML = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a href="${original}" class="gallery__link"><img src="${preview}" alt="${description}" class="gallery__image"></a>`,
+  )
+  .join('');
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
