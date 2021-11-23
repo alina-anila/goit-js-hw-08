b2d79e2e96dce70042084fb1be1d3604977a1e2d
Alina Boiko <alina2boiko@gmail.com>
2021-11-23 19:25:58 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player(document.querySelector('#vimeo-player'));
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(timeupdate, 1000));
+
+if (localStorage.getItem(CURRENT_TIME)) {
+  player.setCurrentTime(Number.parseFloat(localStorage.getItem(CURRENT_TIME)));
+}
+
+function timeupdate(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds === event.duration ? 0 : event.seconds);
+}
